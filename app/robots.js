aedccd091bcd4344b83f20d6f89fee2a6960e9af
chemistleo <lelibol@gmail.com>
2026-05-31 10:41:51 +0300

    robot.js eklendi

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+      rules: {
+        userAgent: '*',
+        allow: '/',
+      },
+      sitemap: 'https://menuqo.com.tr/sitemap.xml',
+    };
+  }
